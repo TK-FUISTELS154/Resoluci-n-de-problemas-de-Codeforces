ad030d8a4e2a47777230e9add1ba0a98817bfa1c
TK-FUISTELS154 <garcialuisfernando9780@gmail.com>
2025-09-30 15:55:47 -0400

    Actualizar 59A.py

--- code Foreces/59A.py
+++ code Foreces/59A.py
@@ -1,12 +1,8 @@
 letra = input()
+mayusculas = sum(1 for c in letra if c.isupper())
+minusculas = sum(1 for c in letra if c.islower())
 
-mayusculas = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
-minusculas = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
-
-max_mayusculas = len(set(letra) & mayusculas)
-max_minusculas = len(set & minusculas)
-
-if max_minusculas<max_mayusculas:
+if mayusculas > minusculas:
     print(letra.upper())
-elif max_mayusculas<=max_minusculas:
+else:
     print(letra.lower())
